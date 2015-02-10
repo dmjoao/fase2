01d9a77557ad41de4ee026318ba0f631971b6ca4
Daniel Joao <dmjoao@gmail.com>
2015-02-10 14:35:03 -0200

    alterando a index

--- index.php
+++ index.php
@@ -1,34 +1,29 @@
-<?php header('HTTP/1.1 404 Not Found'); ?>
-
-<?php
-    function verificaRota($sPagina) {
-        $vPaginas = ["index","inicial","empresa","produtos","servicos","contato"];
-        if(in_array($sPagina,$vPaginas)){
-            if(is_file(__DIR__."/".$sPagina.".php")){
-                if($sPagina == "index")
-                    $sPagina = "inicial";
-                require_once($sPagina.".php");
-            }
-        }else{
-            if($sPagina == "")
-                require_once("inicial.php");
-            else
-                require_once("erro.php");               
-        }
+<?php 
+$vPaginas = [
+    ""         => "inicial.php",
+    "index"    => "inicial.php",
+    "inicial"  => "inicial.php",
+    "empresa"  => "empresa.php",
+    "produtos" => "produtos.php",
+    "servicos" => "servicos.php",
+    "contato"  => "contato.php"
+];
+    	
+function verificaRota($sPagina) {
+    if (array_key_exists($sPagina, $vPaginas) && is_file(__DIR__. "/" . $vPaginas[$sPagina]) {
+	require_once __DIR__ . "/" . $vPaginas[$sPagina];
+    } else {
+        http_response_code(404);
+        require_once "erro.php";
     }
+}
 
-    $vRota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-
-    $sPaginaRota = $vRota['path'];
-    $sPagina = substr($sPaginaRota,1);
-    $sPagina = strtolower($sPagina);
-?>
-
-
-
-	<?php
-	    require_once("includes/cabecalho.php");
-  	    require_once("includes/menu.php");
-	    verificaRota($sPagina);
-	    require_once("includes/rodape.php");
-	?>
+$vRota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+$sPaginaRota = $vRota['path'];
+$sPagina = substr($sPaginaRota,1);
+$sPagina = strtolower($sPagina);
+    
+require_once("includes/cabecalho.php");
+require_once("includes/menu.php");
+verificaRota($sPagina);
+require_once("includes/rodape.php");
